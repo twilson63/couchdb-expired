8d482e5ac9b4b5d7825482a92468e079c02d819e
Tom Wilson <tom@jackhq.com>
2014-01-02 20:10:12 -0500

    fixed tests

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ module.exports = function(config, cb) {
   config = _.extend(_default, config);
   var nano = require('nano')(config.server);
   var remove = require('./lib/remove')(config);
-  // get db list
+  // get db list if config uses asterisk then get all dbs
   config.dbs[0] === "*" ? nano.db.list(process) : process(null, config.dbs);
-
+  // for each db `remove` expired docs
   function process(err, list) { 
     async.map(list, remove, cb); 
   }
