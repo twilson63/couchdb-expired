686611caa0a017b8260f294f603c9c9faa22a852
Tom Wilson <tom@jackhq.com>
2014-01-03 20:21:18 -0500

    fixed cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
-#! /usr/bin/env /usr/local/bin/node
+#! /usr/bin/env node
 
 var config = process.argv[2] ? require(process.argv[2]) : {};
-var expire = require(__dirname + '../');
+var expire = require(__dirname + '/');
 expire(config, function(err, res) {
   if (err) { return console.log(err); }
   console.log(res);
